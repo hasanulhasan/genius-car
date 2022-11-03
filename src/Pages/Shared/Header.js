216ed17a470b2515ef1153e41f5bcf735bc7d5b9
Md Hasanul Karim <hasanlink71@yahoo.com>
2022-11-03 19:42:09 +0600

    signup o

--- src/Pages/Shared/Header.js
+++ src/Pages/Shared/Header.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
+  const menuItems = <>
+    <li className='font-semibold'><Link to='/'>Home</Link></li>
+    <li className='font-semibold'><Link to='/login'>Login</Link></li>
+    <li className='font-semibold'><Link to='/signup'>Signup</Link></li>
+  </>
   return (
     <div>
       <div className="navbar h-20 mb-12 bg-base-100">
@@ -11,10 +16,7 @@ const Header = () => {
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </label>
             <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-              <li><a>Home</a></li>
-              <li><a>Login</a></li>
-              <li><a>Register</a></li>
-              <li><a>About Us</a></li>
+              {menuItems}
               <li tabIndex={0}>
                 <a className="justify-between">
                   Parent
@@ -25,7 +27,7 @@ const Header = () => {
                   <li><a>Submenu 2</a></li>
                 </ul>
               </li>
-              <li><a>Item 3</a></li>
+              <li><a>Item 3 this</a></li>
             </ul>
           </div>
           <Link to='/' className="btn btn-ghost normal-case text-xl">
@@ -34,7 +36,7 @@ const Header = () => {
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal p-0">
-            <li><a>Item 1</a></li>
+            {menuItems}
             <li tabIndex={0}>
               <a>
                 Parent
--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import img from '../../assets/images/login/login.svg'
 
-const Login = () => {
-  const handleLogin = (e) => {
+const Signup = () => {
+  const handleSignUp = (e) => {
     e.preventDefault();
     const form = e.target;
     const name = form.email.value;
     const password = form.password.value;
+    const conPassword = form.confirm - password.value;
     form.reset();
-
   }
   return (
     <div className="hero w-full my-20">
@@ -18,8 +18,8 @@ const Login = () => {
           <img src={img} alt="" />
         </div>
         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 py-16">
-          <form onSubmit={handleLogin} className="card-body">
-            <h1 className="text-5xl font-bold text-center">Login now!</h1>
+          <form onSubmit={handleSignUp} className="card-body">
+            <h1 className="text-5xl font-bold text-center">Sign Up</h1>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
@@ -31,19 +31,22 @@ const Login = () => {
                 <span className="label-text">Password</span>
               </label>
               <input type="text" placeholder="password" name='password' className="input input-bordered" />
+            </div>
+            <div className="form-control">
               <label className="label">
-                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                <span className="label-text">Confirm Password</span>
               </label>
+              <input type="text" placeholder="Confirm Password" name='confirm-password' className="input input-bordered" />
             </div>
             <div className="form-control mt-6">
               <button className="btn btn-primary" type='submit'>Login</button>
             </div>
           </form>
-          <p className='text-center'>New to Genius Car? <Link className='text-orange-600' to='/signup'>Signup</Link></p>
+          <p className='text-center'>Already have an account? <Link className='text-orange-600' to='/login'>Login</Link></p>
         </div>
       </div>
     </div>
   );
 };
 
-export default Login;
+export default Signup;
