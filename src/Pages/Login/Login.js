6498ecb9dc9ce03b9cf9d6ab947eb621875aa1ad
Md Hasanul Karim <hasanlink71@yahoo.com>
2022-11-03 18:57:52 +0600

    firebase install complete

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -2,6 +2,14 @@ import React from 'react';
 import img from '../../assets/images/login/login.svg'
 
 const Login = () => {
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.email.value;
+    const password = form.password.value;
+    form.reset();
+
+  }
   return (
     <div className="hero w-full my-20">
       <div className="hero-content grid gap-20 md:grid-cols-2 flex-col lg:flex-row">
@@ -9,19 +17,19 @@ const Login = () => {
           <img src={img} alt="" />
         </div>
         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <div className="card-body">
+          <form onSubmit={handleLogin} className="card-body">
             <h1 className="text-5xl font-bold text-center">Login now!</h1>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
               </label>
-              <input type="text" placeholder="email" className="input input-bordered" />
+              <input type="text" placeholder="email" name='email' className="input input-bordered" />
             </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Password</span>
               </label>
-              <input type="text" placeholder="password" className="input input-bordered" />
+              <input type="text" placeholder="password" name='password' className="input input-bordered" />
               <label className="label">
                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
               </label>
@@ -29,7 +37,7 @@ const Login = () => {
             <div className="form-control mt-6">
               <button className="btn btn-primary" type='submit'>Login</button>
             </div>
-          </div>
+          </form>
         </div>
       </div>
     </div>
